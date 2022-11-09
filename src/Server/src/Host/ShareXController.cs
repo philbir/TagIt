fbd09180413c1f09c2499a9a01df408cf9813d60
Philippe <tree@gmx.ch>
2022-11-09 06:56:49 +0100

    Update .NET7

---
+++ src/Server/src/Host/ShareXController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TagIt;
+[Route("api/[controller]")]
+[ApiController]
+public class ShareXController : ControllerBase
+{
+}
