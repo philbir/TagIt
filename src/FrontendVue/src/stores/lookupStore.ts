2f6e6875a65e75d5bd04efd53dfea88f4e5d8593
Philippe <tree@gmx.ch>
2022-12-04 13:14:57 +0100

    Missing UI

--- src/FrontendVue/src/stores/lookupStore.ts
+++ src/FrontendVue/src/stores/lookupStore.ts
@@ -1,23 +1,39 @@
 import { graphqlClient } from "./../graphqlClient";
-import { LookupsDocument } from "./../generated/graphql";
+import {
+    LookupsDocument,
+    AddTagDefintionDocument,
+} from "./../generated/graphql";
 import { ref } from "vue";
 import { defineStore } from "pinia";
+import { pipe, subscribe } from "wonka";
 
 export const useLookupStore = defineStore("lookup", () => {
     const isReady = ref(false);
     const thingTypes = ref();
     const tagDefintions = ref();
 
-    const load = async () => {
+    const load = () => {
+        const { unsubscribe } = pipe(
+            graphqlClient.query(LookupsDocument, {}),
+            subscribe((result) => {
+                thingTypes.value = result.data?.thingTypes;
+                tagDefintions.value = result.data?.tagDefintions?.nodes;
+                isReady.value = true;
+            })
+        );
+        /*
         const result = await graphqlClient
             .query(LookupsDocument, {})
-            .toPromise();
-
-        thingTypes.value = result.data?.thingTypes;
-        tagDefintions.value = result.data?.tagDefintions?.nodes;
-
-        isReady.value = true;
+            .toPromise();*/
     };
 
-    return { isReady, thingTypes,tagDefintions, load };
+    const addTagDefintion = async (name: string) => {
+        const result = await graphqlClient
+            .mutation(AddTagDefintionDocument, { input: { name } })
+            .toPromise();
+
+        return result.data?.addTagDefintion.tagDefinition;
+    };
+
+    return { isReady, thingTypes, tagDefintions, load, addTagDefintion };
 });
